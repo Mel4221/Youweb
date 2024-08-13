bb8fb8e540a44724a96433c7cc6fb4fa17ae574f
Melquiceded <melqui4251@gmail.com>
2024-08-13 16:28:59 -0400

    Merging because this state will through an error

---
+++ bin/Debug/net7.0/you/VideoDownloader.py
@@ -0,0 +1,129 @@
+"""
+Copyright (c) <2024> <Melquiceded Balbi Villanueva>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+import moviepy.editor as mp
+import regex
+from pathlib import Path
+import os
+import gc
+from pytube import YouTube
+from Searcher import *
+import emoji
+from Metadata import CreateFileInfo
+import ffmpeg
+from Tools import PrintStatus
+
+
+
+
+
+Attemps = 0 
+"""
+def CreateFileInfo(yt,fileInfo,fileName):
+          a = open(fileInfo,"a")
+          a.write("KEY=VIDEO_FILE;\nVALUE="+fileName+";\n")
+          a.write("KEY=VIDEO_TITLE;\nVALUE="+yt.title+";\n")
+          a.write("KEY=VIDEO_LENGTH;\nVALUE="+str(yt.length)+";\n")
+          a.write("KEY=VIDEO_DESCRIPTION;\nVALUE="+str(yt.description)+";\n")
+          a.write("KEY=VIDEO_THUMBNAIL_URL;\nVALUE="+str(yt.thumbnail_url)+";\n")
+          a.close()
+
+"""
+
+
+
+def remove_emoji(string):
+    s = emoji.replace_emoji(string, replace="")
+    if s.__contains__("/"):
+            s = s.replace("/","")
+    if s.__contains__("\""):
+            s = s.replace("\\")
+    return s 
+    #return emoji.replace_emoji(string, replace='')
+
+def DownloadVideo(videoLink,videoQuality,download_location,tempId):
+        global Attemps
+        #Clear()
+        print("Downloading Video at "+videoQuality)
+        print("Downloading Video: "+videoLink)
+        fileInfo = download_location+tempId+".mp4.info"
+
+        #videoQuality = "1080p"
+        try:
+                yt = YouTube(videoLink)
+                fileName = download_location+regex.sub(r'[^\w]', ' ',yt.title)+".mp4"
+                audio = fileName+".audio"
+                video = fileName+".video"
+                #create a file that contains the info from the video
+                CreateFileInfo(yt,fileInfo,fileName)
+                #lets be honest is just as easy as adding
+                #an extra p so lets just add it and remove this part
+                """
+                if videoQuality=="":
+                        videoQuality = "720"
+                if videoQuality.find("p") == -1:
+                        videoQuality = videoQuality+"p"
+                """
+                PrintStatus(download_location,"50","Downloading... "+yt.title+" Attemp: "+str(Attemps)+" Video Quality: "+videoQuality)
+                
+                #filter = yt.streams.filter(resolution=videoQuality)#.first().download(output_path="downloads/videos/", filename=yt.title+".mp4", filename_prefix="",skip_existing=True, timeout=10000, max_retries=10)
+                filter = yt.streams.filter(resolution=videoQuality,adaptive=True).first().download(output_path=download_location, filename=video, filename_prefix="",skip_existing=False, timeout=35, max_retries=10)
+                
+                PrintStatus(download_location,"75","Downloading Audio... "+yt.title)  
+                yt.streams.filter(only_audio=True).first().download(output_path=download_location, filename=audio, filename_prefix="",skip_existing=False, timeout=35, max_retries=10)
+                
+                PrintStatus(download_location,"100","Mixing Video and Audio...")
+                #yt.streams.get_by_itag(22).download(output_path="/home/mel/Music/", filename="test.mp4", filename_prefix="",skip_existing=True, timeout=15, max_retries=5)
+                video_stream = ffmpeg.input(video)
+                audio_stream = ffmpeg.input(audio)
+                
+                ffmpeg.output(audio_stream, video_stream,fileName).run(overwrite_output=True)
+                
+                PrintStatus(download_location,"100","File Location: "+fileName)
+                #.get_lowest_resolution().download(output_path="downloads/videos/", filename="video.mp4", filename_prefix="",skip_existing=True, timeout=10000, max_retries=10)
+                sucess = open(fileInfo+".sucess","a",encoding="utf-8")
+                sucess.close()
+        except Exception as e:
+                print(e)
+                msg = "Attempting again due to an error...: "+str(e)
+                PrintStatus(download_location,"100",msg)
+                gc.collect()
+                #global Attemps
+                Attemps = Attemps + 1
+                if Attemps == 1:
+                        DownloadVideo(videoLink,videoQuality,download_location,tempId)
+                        return
+                if Attemps == 2:
+                        msg = "Download Quality Set to Default '720p' Due to TOO MANY ATTEMPS\n"+"Please try a lower Video Quality if the video keeps Attempting..."
+                        PrintStatus(download_location,"100",msg)
+                        DownloadVideo(videoLink,"720p",download_location,tempId)
+                        return
+                if Attemps == 3:
+                        msg = "Download Quality Set to Default '480p' Due to TOO MANY ATTEMPS\n"+"Please try a lower Video Quality if the video keeps Attempting..."
+                        PrintStatus(download_location,"100",msg)
+                        DownloadVideo(videoLink,"480p",download_location,tempId)
+                        return
+                if Attemps == 4:
+                        msg = "Download Quality Set to Default '360p' Due to TOO MANY ATTEMPS\n"+"Please try a lower Video Quality if the video keeps Attempting..."
+                        PrintStatus(download_location,"100",msg)
+                        DownloadVideo(videoLink,"360p",download_location,tempId)
+                        return
+                if Attemps == 5:
+                        msg = "Download Quality Set to Default '144p' Due to TOO MANY ATTEMPS\n"+"Please try a lower Video Quality if the video keeps Attempting..."
+                        PrintStatus(download_location,"100",msg)
+                        DownloadVideo(videoLink,"144p",download_location,tempId)
+                        return
+                fail = open(fileInfo+".fail","a",encoding="utf-8")
+                fail.write(str(e))
+                fail.close()
+        return
+                
+        #<Stream: itag="22" mime_type="video/mp4" res="720p" fps="30fps" vcodec="avc1.64001F" acodec="mp4a.40.2" progressive="True" type="video">,
+        #().download(output_path="downloads/video/", filename="video", filename_prefix="",skip_existing=True, timeout=10000, max_retries=10)
+      
---
+++ bin/Debug/net7.0/you/t.py
@@ -0,0 +1,88 @@
+import moviepy.editor as mp
+from pathlib import Path
+from pytube import YouTube
+from VideoDownloader import remove_emoji
+from pathvalidate import sanitize_filepath
+from Metadata import CreateFileInfo
+from Tools import *
+import eyed3,io
+from eyed3.id3.frames import ImageFrame
+import urllib.request
+import emoji
+import ffmpeg
+from Sync import SyncIcon
+
+
+
+def T():
+        
+        return
+        print(str(HasIcon("test.mp3")))
+        return
+        SyncIcon("adele.mp3","Adele - Skyfall (Lyrics)","skyfall")
+        return
+        link = "https://www.youtube.com/watch?v=YQHsXMglC9A"
+       
+        download_location = ""
+        fileName = "test"
+        icon = fileName+".jpg"
+        video = fileName+".mp4"
+        audio = fileName+".audio"
+        mp3_temp = fileName+"_temp"+".mp3"
+        mp3File = fileName+".mp3"
+       
+        print("LINK: "+link)
+        yt = YouTube(link) # or any other YouTube-link
+        print(yt.title)
+        """
+        print(yt.metadata) 
+        print(yt.metadata[0]['Song']) 
+        print(yt.metadata[0]['Artist'])   
+        print(yt.metadata[0]['Album']) 
+        """
+        
+        print("Downloading Icon...: "+str(yt.thumbnail_url))
+        urllib.request.urlretrieve(yt.thumbnail_url, icon)
+        print("Downloading Video...: "+yt.title)
+        yt.streams.filter(file_extension='mp4').get_lowest_resolution().download(output_path=download_location, filename=video, filename_prefix="",
+        skip_existing=False, timeout=300000, max_retries=50)
+        
+        #some of this will be uncommented but mainly the PrintStatus
+        #PrintStatus(download_location,"95","Binding Video and Audio...")
+        #yt.streams.get_by_itag(22).download(output_path="/home/mel/Music/", filename="test.mp4", filename_prefix="",skip_existing=True, timeout=15, max_retries=5)
+        """
+        print("Binding Audio and Video...: "+yt.title)
+        video_stream = ffmpeg.input(video)
+        audio_stream = ffmpeg.input(audio)
+
+        ffmpeg.output(audio_stream, video_stream,mp3_temp).run(overwrite_output=True)
+        """
+         
+        print("Converting to mp3...: "+yt.title+".mp3") 
+        mp3 = mp.VideoFileClip(video)
+        mp3.audio.write_audiofile(Path(mp3File))
+
+        print("Adding Thumbnail!!!...: "+mp3File+".mp3") 
+        audiofile = eyed3.load(mp3File)
+        audiofile.initTag(version=(2, 3, 0))  # version is important
+        # Other data for demonstration purpose only (from docs)
+        audiofile.tag.album_artist = "Various Artists"
+        audiofile.tag.title = yt.title
+
+        audiofile.tag.images.set(ImageFrame.FRONT_COVER, open(icon,'rb').read(), 'image/jpeg')
+
+        audiofile.tag.save()
+        print("Donwload completed Successfully!!!")
+        """
+        yt = YouTube(link) # or any other YouTube-link
+
+        filter = yt.streams.filter(file_extension='mp4').get_lowest_resolution().download(output_path=download_location, filename=fileName, filename_prefix="",
+        skip_existing=True, timeout=300000, max_retries=50)
+        
+        link = "https://www.youtube.com/watch?v=t7wIg8wfNzs"
+        print("LINK: "+link)
+        yt = YouTube(link) # or any other YouTube-link
+        print(yt.streams.get_highest_resolution())
+        #print(yt.streams.order_by('resolution').desc().first())
+        """
+T()
